fbe7f9faffd41ed83a002cbbc60ebdf31dbfb82c
bartonchik05@gmail.com <bartonchik05@gmail.com>
2024-10-03 16:54:25 +0800

    button animation completed

--- app/components/TryItFreeButton.tsx
+++ app/components/TryItFreeButton.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import "@/app/styles.css";
 export default function TryItFreeButton() {
   return (
-    <button className="relative bg-green-600 text-white px-7 py-2 rounded-xl w-[140px] h-[40px] overflow-hidden group">
-      <span className="relative z-10 group-hover:hidden">Try it FREE!</span>
-      <Image
-        src="/images/icons/cart.svg"
-        width={20}
-        height={20}
-        className="absolute right-1/2 top-1/2 transform translate-x-1/2 -translate-y-1/2 hidden group-hover:block group-hover:z-1 bg-blue-300 p-1 rounded-full"
-        alt="Try it FREE!"
-      ></Image>
-      <span className="absolute inset-0 bg-blue-500 transform scale-0 group-hover:scale-100 transition-transform duration-300 ease-out origin-center rounded-xl group-hover:z-0"></span>
-    </button>
+    <>
+      <button className="relative bg-green-600 text-white px-7 py-2 rounded-xl w-[160px] h-[40px] overflow-hidden group">
+        <span className="z-10 group-hover:hidden">Try it FREE!</span>
+        <div className="flex justify-center absolute inset-0 bg-blue-500 transform scale-0 group-hover:scale-100 transition-transform duration-300 ease-out group-hover:z-0 ">
+          <div className="flex justify-center items-center align-middle group-hover:duration-900">
+            <Image
+              src="/images/icons/cart.svg"
+              width={30}
+              height={30}
+              className="p-[8px] bg-blue-100 scale-0  group-hover:scale-100 transition-transform rounded-full group-hover:z-10 group-hover:delay-300 duration-700"
+              alt="Try it FREE!"
+            ></Image>
+          </div>
+        </div>
+      </button>
+    </>
   );
 }
