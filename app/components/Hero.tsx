1b5103edcd2eecda63e5e61d0091bf695849eb42
bartonchik05@gmail.com <bartonchik05@gmail.com>
2024-10-03 13:58:42 +0800

    wip

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import TryItFreeButton from "./TryItFreeButton";
 export default function Hero() {
   return (
-    <div className="flex flex-wrap justify-between">
-      <div className="flex flex-col gap-4 sm:w-[50%]">
+    <div className="flex flex-wrap  align-middle items-center">
+      <div className="flex flex-col gap-4 md:w-[43%]">
         <p className="text-[60px] font-bold tracking-tight leading-none text-center">
           The Chatbot, Automation and CRM platform built for Agencies
         </p>
@@ -14,52 +14,62 @@ export default function Hero() {
           to streamline and supercharge your client solutions.
         </p>
         <div className="flex gap-4 justify-between px-4">
-          <Image
-            src="/images/icons/whatsapp.svg"
-            alt="heroImg"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/images/icons/ig.svg"
-            alt="heroImg"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/images/icons/messenger.svg"
-            alt="heroImg"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/images/icons/telegram.svg"
-            alt="heroImg"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/images/icons/call.svg"
-            alt="heroImg"
-            width={30}
-            height={30}
-          />
-          <Image
-            src="/images/icons/sms.svg"
-            alt="heroImg"
-            width={30}
-            height={30}
-          />
+          {[
+            "/images/icons/whatsapp.svg",
+            "/images/icons/ig.svg",
+            "/images/icons/messenger.svg",
+            "/images/icons/telegram.svg",
+            "/images/icons/call.svg",
+            "/images/icons/sms.svg",
+          ].map((path, index) => (
+            <Image
+              key={index}
+              src={path}
+              alt={`Icon ${index + 1}`}
+              width={30}
+              height={30}
+            />
+          ))}
         </div>
         <TryItFreeButton />
       </div>
-      <div className="h-[100vh]">
-        <Image
-          src="/images/hero/hero.svg"
-          alt="heroImg"
-          width={100}
-          height={100}
-        />
+      <div className="h-[100vh] flex space-x-24 m-12">
+        {/* <div className="absolute flex gap-4 flex-col ">
+          {[
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+          ].map((path, index) => (
+            <Image
+              key={index}
+              src={path}
+              alt={`Image ${index + 1}`}
+              width={180}
+              height={100}
+            />
+          ))}
+        </div>
+        <div className="absolute flex gap-4 flex-col">
+          {[
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+            "/images/message/instagram chat agent.svg",
+          ].map((path, index) => (
+            <Image
+              key={index}
+              src={path}
+              alt={`Image ${index + 1}`}
+              width={180}
+              height={100}
+            />
+          ))}
+        </div> */}
       </div>
     </div>
   );
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,20 +5,42 @@ import TryItFreeButton from "./TryItFreeButton";
 const Navbar = () => {
   return (
     <>
-      <div className="flex justify-around items-center row-screen p-4">
-        <Image
-          src="/images/logo.png"
-          alt="logo"
-          width={180}
-          height={40}
-          style={{ maxWidth: "73vw" }}
-        />
-        <Link href={"/overview"}>Overview</Link>
-        <Link href={"/whitelabel"}>Whitelabel</Link>
-        <Link href={"/pricing"}>Pricing</Link>
-        <Link href={"/"}>Features</Link>
-        <button>EN</button>
-        <TryItFreeButton />
+      <div className="flex justify-between items-center row-screen p-4">
+        <div className="flex justify-normal gap-12 items-center row-screen p-4">
+          <Image
+            src="/images/logo.png"
+            alt="logo"
+            width={180}
+            height={40}
+            style={{ maxWidth: "73vw" }}
+          />
+          <Link href={"/overview"}>Overview</Link>
+          <Link href={"/whitelabel"}>Whitelabel</Link>
+          <Link href={"/pricing"}>Pricing</Link>
+          <div className="flex gap-2 items-center">
+            Features
+            <Image
+              src="/images/icons/arrow-down.svg"
+              alt=""
+              width={20}
+              height={20}
+              //<Link href={"/"}>Features</Link>
+            />
+          </div>
+        </div>
+        <div className="flex gap-8 px-12">
+          <div className="flex gap-2 items-center">
+            <Image
+              src="/images/icons/arrow-down.svg"
+              alt=""
+              width={20}
+              height={20}
+              //<Link href={"/"}>Features</Link>
+            />
+            <button>EN</button>
+          </div>
+          <TryItFreeButton />
+        </div>
       </div>
     </>
   );
