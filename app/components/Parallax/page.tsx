08537945dd78c74e2d7789d5a4c31b92ccdc9ce5
gk6294 <bartonchik05@gmail.com>
2024-11-07 00:00:53 +0800

    update

--- app/components/Parallax/page.tsx
+++ app/components/Parallax/page.tsx
@@ -10,16 +10,18 @@ import {} from "@/app/interfaces";
 const Parallax = (props: ParallaxContextsInterface) => {
   return props.ParallaxContext!.map(
     (content: ParallaxContextInterface, index: number) => (
-      <motion.div key={index} className=" sticky top-0 h-[100vh]">
+      <motion.div
+        key={index}
+        className=" sticky top-0 h-[100vh]"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        viewport={{ amount: "all" }}
+      >
         <div className="h-[300px]  bg-red-500  mt-[50vh]">
-          <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ amount: "all" }}
-            className="ounded-3xl h-[500px]  object-cover"
-          >
+          <div className="rounded-3xl h-[500px]  object-cover">
             <Image src={content.image} alt="image" height={100} width={100} />
-          </motion.div>
+          </div>
         </div>
         <p>
           {content.title},{content.header},{content.description}
