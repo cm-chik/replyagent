d853bd36a0e24851c8dbe0cd6874ad073e908ba9
barton05 <bartonchik05@gmail.com>
2024-11-09 02:36:13 +0800

    wip

--- app/components/Carousel/Context.tsx
+++ app/components/Carousel/Context.tsx
@@ -1,18 +1,70 @@
 import { CarouselCardInterface } from "@/app/interfaces";
-import AnimatedButton from "../AnimatedButton";
-
 const CarouselCardContext: CarouselCardInterface = {
- carousel_title: "Integrate with virtually anything!",
-       carousel_description: "Easily integrate with external applications using our APIs and Webhooks, or leverage our native integrations with Cal.com, Dify.ai, Microsoft Text to Speech, Twilio, Cloudinary, ChatGPT, Active Campaign, and a huge library of 1600+ additional options via Make.com!",
-       carousel_row: (
-        <div className="flex gap-[1rem]">
-          <AnimatedButton title="Learn More" icon="/images/icons/learnmore.svg" />
-          <AnimatedButton title="Learn More" icon="/images/icons/learnmore.svg" />
-          <AnimatedButton title="Learn More" icon="/images/icons/learnmore.svg" />
-          <AnimatedButton title="Learn More" icon="/images/icons/learnmore.svg" />
-          <AnimatedButton title="Learn More" icon="/images/icons/learnmore.svg" />
-          </div>
-      ),
+  carousel_title: "Integrate with virtually anything!",
+  carousel_description:
+    "Easily integrate with external applications using our APIs and Webhooks, or leverage our native integrations with Cal.com, Dify.ai, Microsoft Text to Speech, Twilio, Cloudinary, ChatGPT, Active Campaign, and a huge library of 1600+ additional options via Make.com!",
+  carousel_row: [
+    {
+      carousel_image: "/images/icons/telegram.svg",
+      carousel_word: "Telegram",
+    },
+    {
+      carousel_image: "/images/icons/messenger.svg",
+      carousel_word: "Messenger",
+    },
+    {
+      carousel_image: "/images/icons/twilio.svg",
+      carousel_word: "Twilio",
+    },
+    {
+      carousel_image: "/images/icons/whatsapp.svg",
+      carousel_word: "WhatsApp",
+    },
+    {
+      carousel_image: "/images/icons/cal.com",
+      carousel_word: "Cal.com",
+    },
+    {
+      carousel_image: "/images/icons/difyai.png",
+      carousel_word: "Dify.ai",
+    },
+    {
+      carousel_image: "/images/icons/make.svg",
+      carousel_word: "Make.com",
+    },
+    {
+      carousel_image: "/images/icons/chatgpt.png",
+      carousel_word: "ChatGPT",
+    },
+    {
+      carousel_image: "/images/icons/microsofttts.png",
+      carousel_word: "Microsoft TTS",
+    },
+    {
+      carousel_image: "/images/icons/ig.svg",
+      carousel_word: "Instagram",
+    },
+    {
+      carousel_image: "/images/icons/sms.svg",
+      carousel_word: "SMS",
+    },
+    {
+      carousel_image: "/images/icons/woocommerce.svg",
+      carousel_word: "WooCommerce",
+    },
+    {
+      carousel_image: "/images/icons/cloudinary.svg",
+      carousel_word: "Cloudinary",
+    },
+    {
+      carousel_image: "/images/icons/shopify.svg",
+      carousel_word: "Shopify",
+    },
+    {
+      carousel_image: "/images/icons/hotmart.svg",
+      carousel_word: "Hotmart",
+    },
+  ],
 };
 
 export default CarouselCardContext;
--- app/components/Carousel/page.tsx
+++ app/components/Carousel/page.tsx
@@ -1,22 +1,49 @@
 import React from "react";
-
-import type {
-  CarouselCardInterface,
-} from "@/app/interfaces";
+import Image from "next/image";
+import type { CarouselCardInterface } from "@/app/interfaces";
+import { motion } from "framer-motion";
 
 const CarouselCard = (props: CarouselCardInterface) => {
   return (
-<div className="px-12">
+    <div className="px-12">
       <div className="flex flex-col text-center leading-[4rem] space-y-[4rem]">
         <div className="text-[4rem] font-bold tracking-tighter py-2 ">
           {props.carousel_title}
         </div>
-        <div className="leading-normal md:px-[14rem]">{props.carousel_description}</div>
+        <div className="leading-normal md:px-[14rem]">
+          {props.carousel_description}
+        </div>
         <div className="flex w-[100%] max-md:flex-col justify-center gap-[2rem] max-md:space-y-[2rem] px-10">
-          <div>
-            {props.carousel_row}
+          <motion.div
+            animate={{ x: ["0%", "-100%"] }}
+            transition={{ duration: 40, ease: "linear", repeat: Infinity }}
+            style={{ display: "inline-block" }} // Ensure the content is inline for horizontal scrolling
+          >
+            <div className="flex gap-2  space-x-[4rem] justify-center items-center">
+              {props.carousel_row.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col space-y-[3rem]"
+                  style={{
+                    position: "relative",
+                    width: "40px",
+                    height: "40px",
+                  }}
+                >
+                  <Image
+                    src={item.carousel_image}
+                    alt={item.carousel_word}
+                    fill
+                    style={{ objectFit: "contain" }}
+                  />
+                  <div className="text-[0.5rem] leading-none w-[40px] text-center">
+                    {item.carousel_word}
+                  </div>
+                </div>
+              ))}
             </div>
-      </div>
+          </motion.div>
+        </div>
       </div>
     </div>
   );
