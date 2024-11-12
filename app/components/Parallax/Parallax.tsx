7a5ec68e175ee005e219c278b3578fa4e3ee6183
gk6294 <bartonchik05@gmail.com>
2024-11-12 13:36:37 +0800

    desktop ✅

--- app/components/Parallax/Parallax.tsx
+++ app/components/Parallax/Parallax.tsx
@@ -9,19 +9,19 @@ import { ParallaxFullScreenAnimation } from "@/app/animations/animation";
 const Parallax = (props: ParallaxContextsInterface) => {
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col py-[4rem] w-[120rem] gap-y-[4rem]">
+      <div className="flex flex-col py-[4rem] w-screen gap-y-[4rem]">
         {props.ParallaxContext!.map(
           (content: ParallaxContextInterface, index: number) => (
            <ParallaxFullScreenAnimation key={index}>
-            <div className="flex justify-between" >
-              <div className="flex flex-col justify-center w-[50rem]">
+            <div className="flex justify-around" >
+              <div className="flex flex-col justify-center w-[40rem]">
                   <div>{content.title}</div>
                   <div className="text-[4.5rem] font-bold leading-none py-[2rem]">
                     {content.header}
                   </div>
                   <div>{content.description}</div>
               </div>
-              <div className="w-[30rem]" >
+              <div className="w-[20rem]" >
                 <Image src={content.image} alt="image" width={400} height={800}/>
               </div>
             </div>
